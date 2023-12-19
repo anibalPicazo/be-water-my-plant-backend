870669b4ed098ef1df5411ae823b9799ebfde26a
anibal.picazo <anibal.picazo@securitasdirect.es>
2023-12-19 17:13:13 +0100

    cors

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const { dbConnection } = require('./database/config')
 const cors = require('cors')
 const app = express()
-
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Authorization',
+};
 require('dotenv').config();
 
 app.use(express.static('public'))
@@ -11,7 +17,7 @@ app.use(express.json())
 
 dbConnection()
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/exercise', require('./routes/exercise'))
